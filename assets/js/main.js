9ae66e1a973e32d8b850be244ea90486415b6f97
Nykenik24 <Nykenik24@users.noreply.github.com>
2026-07-21 19:45:04 +0000

    deploy: 971203b1cc5f943ffd19f5e839e28a44d4c6c3d6

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+const elements = document.querySelectorAll(".my-age");
+
+/* I wasn't actually born at 00:00, I just
+ * don't know at what hour I was exactly born.
+ */
+const bday = new Date("2012-04-22T00:00:00");
+
+const updateAge = () => {
+  const currentDate = new Date();
+
+  let years = currentDate.getFullYear() - bday.getFullYear();
+  let months = currentDate.getMonth() - bday.getMonth();
+  let days = currentDate.getDate() - bday.getDate();
+
+  // Borrow days from previous month
+  if (days < 0) {
+    months--;
+
+    const prevMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      0
+    );
+    days += prevMonth.getDate();
+  }
+
+  // Borrow months from previous year
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+
+  let nextBirthday = new Date(bday);
+  nextBirthday.setFullYear(currentDate.getFullYear());
+
+  if (currentDate > nextBirthday) {
+    nextBirthday.setFullYear(currentDate.getFullYear() + 1);
+  }
+
+  const daysLeft = Math.floor(
+    (nextBirthday - currentDate) / (1000 * 60 * 60 * 24)
+  );
+
+
+  const weeks = Math.floor(days / 7);
+  days %= 7;
+
+  const singOrPlural = (n, base) => {
+    return n > 1 ? `${base}s` : base
+  }
+
+  elements.forEach((e) => {
+    e.textContent = `${years} years, ${months} ${singOrPlural(months, "month")}, ${weeks} ${singOrPlural(weeks, "week")} and ${days} ${singOrPlural(days, "day")} old`;
+    e.title = `${daysLeft} days left`;
+  });
+
+  requestAnimationFrame(updateAge);
+};
+
+updateAge();
